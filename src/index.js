eb93497992cd081d1fe13aca77b9e67e6671408f
Joonas Ruuth <ruuthjoonas@gmail.com>
2024-12-17 10:19:47 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// Function to send event
+function sendSalesforceEvent() {
+  SalesforceInteractions.sendEvent({
+    user: {
+      identities: {
+        loyaltyId: "885627312393",
+      },
+      attributes: {
+        firstName: "Joe",
+        lastName: "Smith",
+        email: "joe.smith@domain.com",
+      },
+    },
+  });
+}
+
+sendSalesforceEvent();
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
