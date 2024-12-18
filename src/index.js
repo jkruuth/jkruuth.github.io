4b000cc16994ce3785dbfab067102c4e7590bc43
Joonas Ruuth <ruuthjoonas@gmail.com>
2024-12-18 09:31:09 +0200

    test

--- src/index.js
+++ src/index.js
@@ -3,24 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// Function to send event
-function sendSalesforceEvent() {
-  window.SalesforceInteractions.sendEvent({
-    user: {
-      identities: {
-        loyaltyId: "885627312393",
-      },
-      attributes: {
-        firstName: "Joe",
-        lastName: "Smith",
-        email: "joe.smith@domain.com",
-      },
-    },
-  });
-}
-
-sendSalesforceEvent();
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
